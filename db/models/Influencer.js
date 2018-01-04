6f079ce4d25c09c704fb6da832fd76e7dd1b0eb0
DenimCity <altidorj1@gmail.com>
2018-01-04 15:59:32 -0500

    created this file for influncer communication

---
+++ db/models/Influencer.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = require('../schema')
+
+const User = mongoose.model('Influencer', Schema.InfluencerSchema)
+
+
+
+
+
+
+
+module.exports = Influencer
