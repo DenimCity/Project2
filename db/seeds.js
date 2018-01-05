a4c450c26738dd23ab780b2aaeeb4a92c5c1b6a3
DenimCity <altidorj1@gmail.com>
2018-01-04 20:31:08 -0500

    refactoing the seeds file

--- db/seeds.js
+++ db/seeds.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
 
+const User = require('./models/User')
+const Influencer = require('./models/Influencer')
+const Style = require('./models/Style')
 const mongoose = require('mongoose')
 
 // connect to database
@@ -16,58 +19,47 @@ mongoose.connect(process.env.MONGODB_URI)
     `)
     process.exit(-1)
   })
-const User = require('./routes/Users')
-const styles = require('/routes')
+
 
 mongoose.Promise = global.Promise
+//seed file is all just dumby date to preload you database
+//how i delete the users , and make some fake one to test that my files will save to my database
+User.remove ({
 
-
-const Jean = new User({
-  firstName: 'Jean',
-  lastName: 'Altidor',
-  instragramUrl:'https://www.instagram.com/jeanlikedenim/',
-  twitterUrl:'https://twitter.com/jeanlikedenim',
-  location:'Atlanta, Georgia',
-  photoUrl: 'https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/19436484_294868254308560_4898463097905217536_n.jpg',
+})
+.then(()=> {
+  const Jean = new User({
+    firstName: 'Jean',
+    lastName: 'Altidor',
+    instragramUrl:'https://www.instagram.com/jeanlikedenim/',
+    twitterUrl:'https://twitter.com/jeanlikedenim',
+    location:'Atlanta, Georgia',
+    photoUrl: 'https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/19436484_294868254308560_4898463097905217536_n.jpg',
     
-  
-  
-  styles: [{
-    look1: 'https://imgur.com/qaOjfN9',
-    look2:'https://imgur.com/Q5LuoCo',
-    
-  }]
 })
 
-const Eman = new User({
-  firstName: 'Emanuella',
-  lastName: 'Altidor',
-  instragramUrl:'https://www.instagram.com/theonly_ea/',
-  twitterUrl:'https://twitter.com/theonly_EA',
-  location:'Atlanta, Georgia',
-  photoUrl: 'https://imgur.com/KzXEukR',
-  
-  styles: [{
-    look1: 'https://imgur.com/S2YWvh5',
-    look2:'https://i.imgur.com/pV1R2Xv.jpg',
+
+})
+.then(()=>{
+  const Eman = new User({
+    firstName: 'Emanuella',
+    lastName: 'Altidor',
+    instragramUrl:'https://www.instagram.com/theonly_ea/',
+    twitterUrl:'https://twitter.com/theonly_EA',
+    location:'Atlanta, Georgia',
+    photoUrl: 'https://imgur.com/KzXEukR',
     
-    
-  }]
+
 })
 
-User.remove()
-  .then(() => {
-    return User.remove()
-  })
-  .then(() => {
-    console.log('seeds are planted')
-    return Jean.save()
-  })
-  .then(() => {
-    console.log('seeds are planted')
-    return Eman.save()
-    mongoose.connection.close()
-  })
-  .catch(err => {
-    console.log(err)
-  })
+.catch((error) => {
+  console.log('!!!!! ERROR SAVING SEEDED DATA !!!!!')
+  console.log(error)
+}).then(() => {
+  mongoose.connection.close()
+  console.log(`
+      Finished seeding database...
+      
+      Disconnected from MongoDB
+    `)
+})
