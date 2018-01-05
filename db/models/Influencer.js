50efe4bd027d301a835e73e9a958ec7f2acb4288
DenimCity <altidorj1@gmail.com>
2018-01-05 10:28:49 -0500

    refactored

--- db/models/Influencer.js
+++ db/models/Influencer.js
@@ -1,12 +1,5 @@
 const mongoose = require('mongoose')
 const Schema = require('../schema')
-
 const Influencer = mongoose.model('Influencer', Schema.InfluencerSchema)
-
-
-
-
-
-
 
 module.exports = Influencer
