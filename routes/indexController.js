9ca9a4576e6cac15523d952ba4b3ea67a0d30ad7
DenimCity <altidorj1@gmail.com>
2018-01-05 09:31:52 -0500

    testing the index route

--- routes/indexController.js
+++ routes/indexController.js
@@ -3,7 +3,7 @@ const router = express.Router({mergeParams: true})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
+  // res.render('index', { title: 'Express' })
   res.show('I think this is a way of showing that this route works')
 })
 
