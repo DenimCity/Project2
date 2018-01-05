be788917e0e6ba96a402b27ff94e4487ffb9075d
DenimCity <altidorj1@gmail.com>
2018-01-04 20:33:35 -0500

    changed the name

--- routes/usersController.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-/* GET Users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource')
-})
-
-module.exports = router
