99825187ad7c6d54be4a15c2481ce8e03f342750
DenimCity <altidorj1@gmail.com>
2018-01-04 23:45:25 -0500

    testing to see if the index router workds, refactored

--- routes/indexController.js
+++ routes/indexController.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router({mergeParams: true})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' })
+  res.show('I think this is a way of showing that this route works')
 })
 
 module.exports = router
