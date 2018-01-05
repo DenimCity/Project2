068f69b89d50eaa27c4bd58a3b8a36c074f175df
DenimCity <altidorj1@gmail.com>
2018-01-05 13:50:20 -0500

    this route dispalys the index page

--- routes/userController.js
+++ routes/userController.js
@@ -2,11 +2,18 @@ const express = require('express')
 const router = express.Router({mergeParams: true})
 const User = require('../db/models/User')
 
-/* GET Users listing. */
-router.get('/users', function(req, res, next) {
-  
-  res.send('from the user controller')
-  
+
+router.get('/', (request, response) => {
+  User.find({})
+    .then((users) => {
+      response.render('users/index', {
+        users,
+        pageTitle: 'Banana'
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 })
 
 module.exports = router
