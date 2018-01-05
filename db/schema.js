5ad636eaf30dc8e439220319800e28929b6a30d5
DenimCity <altidorj1@gmail.com>
2018-01-04 20:30:49 -0500

    refactored schema, top level reading from the botom. User with influencer embedded, and infuencer has styles embed

--- db/schema.js
+++ db/schema.js
@@ -1,13 +1,88 @@
-import { url } from 'inspector';
-
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 
 //promises "then" and "catch" variables
 mongoose.Promise = global.Promise;
 
 
+const StyleSchema = new Schema({
+  top: {
+    type: String,
+    require: [true, 'What brand of string?']
+  },
+  topInfo: {
+    type: String,
+    required: [true, 'where did you buy the shirt?']
+  },
+  img1: {
+    type: String,
+    required: [true, 'show us what it looks like, upload a picture link']
+  },
+  bottom: {
+    type: String,
+    required: [true, 'What brand are the shorts?']
+  },
+
+  bottomInfo: {
+    type: String,
+    require: [true, 'where did you buy the shirt?']
+  },
+  img2: {
+    type: String,
+    required: [true, 'copy the link of the image here']
+  },
+  shoe: {
+    type: String,
+    required: [true, 'whats the brand of the shoe?']
+  },
+
+  img3: {
+    type: String,
+    required: [true, 'upload a picture of the shoe']
+  },
+
+  userUpload: {
+    type: String,
+    required: [true]
+  }
+}, 
+
+{
+  timestamps: {},
+  usePushEach: true
+}
+)
+
+
+const InfluencerSchema = new Schema({
+
+  name: {
+    type: String,
+    require: [true, 'we need a name for this influencer']
+  },
+  instagramUrl: {
+    type: String,
+    required: [true, 'most influencers have instagrams, copy their link here']
+  },
+
+  location: {
+    type: String,
+    require: [true, 'what city are they from or post majority of their posts']
+  },
+  photoUrl: {
+    type: String,
+    default: 'https://i.imgur.com/Q5LuoCo.jpg'
+  },
+
+  styles: [StyleSchema]
+
+},
+  {
+    timestamps: {},
+    usePushEach: true
+  }
+)
 
 const UserSchema = new Schema({
   username: {
@@ -24,12 +99,12 @@ const UserSchema = new Schema({
     required: [true, "Must have a Last Name"]
   },
   instagramUrl: {
-    type: url,
+    type: String,
     required: [true, 'we need you to have at least a instagram page'],
     unique: [true, 'this twitter page is already in use']
   },
   twitterUrl: {
-    type: url,
+    type: String,
     required: [true, 'we have monthly shoutouts on twitter, so we need your twitter handle'],
     unique: [true, 'this twitter page is already in use']
   },
@@ -40,7 +115,7 @@ const UserSchema = new Schema({
 
   },
   photoUrl: {
-    type: url,
+    type: String,
     default: 'http://images.clipartpanda.com/silhouettes-clipart-clip_art_silhouette_of_a_cartoon_baby_sitting_down_with_his_arms_out_0515-1002-0103-4312_SMU.jpg',
   },
 
@@ -54,93 +129,8 @@ const UserSchema = new Schema({
 )
 
 
-const InfluencerSchema = new Schema({
-
-  name: {
-    type: String,
-    require: [true, 'we need a name for this influencer']
-  },
-  instagramUrl: {
-    type: url,
-    required: [true, 'most influencers have instagrams, copy their link here']
-  },
-
-  location: {
-    type: String,
-    require: [true, 'what city are they from or post majority of their posts']
-  },
-  photoUrl: {
-    type: url,
-    default: 'https://i.imgur.com/Q5LuoCo.jpg'
-  },
-
-  styles: [StyleSChema]
-
-},
-  {
-    timestamps: {},
-    usePushEach: true
-  }
-)
-
-
-
-const StylesSchema = new Scehma({
-  top: {
-    type: String,
-    require: [true, 'What brand of string?']
-  },
-  topInfo: {
-    type: String,
-    required: [true, 'where did you buy the shirt?']
-  },
-  img1: {
-    type: url,
-    required: [true, 'show us what it looks like, upload a picture link']
-  },
-  bottom: {
-    type: String,
-    required: [true, 'What brand are the shorts?']
-  },
-
-  bottomInfo: {
-    type: String,
-    require: [true, 'where did you buy the shirt?']
-  },
-  img2: {
-    type: url,
-    required: [true, 'copy the link of the image here']
-  },
-  shoe: {
-    type: String,
-    required: [true, 'whats the brand of the shoe?']
-  },
-
-  img3: {
-    type: url,
-    required: [true, 'upload a picture of the shoe']
-  },
-
-  userUpload: {
-    type: url,
-    required: [true]
-  }
-}, 
-
-{
-  timestamps: {},
-  usePushEach: true
-}
-)
-
-
-
-
-const UserModel = mongoose.model("Users", UserSchema)
-const styleModel = mongoose.model("Styles", StylesSchema)
-
 module.exports = {
-  User: UserSchema,
-  Style: StyleSChema,
-  Influencer: InfluencerSchema,
+  UserSchema,
+  StyleSchema,
+  InfluencerSchema,
 }
