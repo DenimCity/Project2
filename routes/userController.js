38d4c08f279aeb7c457909a141c3b1c25dc398e7
DenimCity <altidorj1@gmail.com>
2018-01-05 19:31:02 -0500

    adding my users route

--- routes/userController.js
+++ routes/userController.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const router = express.Router({mergeParams: true})
+const router = express.Router({ mergeParams: true })
 const User = require('../db/models/User')
 
-
-router.get('/', (request, response) => {
+///when the user types localhost3000/user this page loads 
+///using this command
+router.get('/', (req, res) => {
   User.find({})
     .then((users) => {
-      response.render('users/index', {
+      res.render('users/index', {
         users,
         pageTitle: 'Banana'
       })
@@ -15,14 +16,88 @@ router.get('/', (request, response) => {
       console.log(error)
     })
 })
+//the route to create a new  new user
+router.get('/new', (req, res) => {
+  response.render('users/new', { pageTitle: 'New User' })
+})
+
+
+
+// ///takign the users info and pushing int othe fiels that you required 
+// router.post('/', (req, res)=>{
+//   const newUser = new User ({
+//     username:req.body.username,
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//         instagramUrl: req.body.instagramUrl,
+//         twitterUrl: req.body.twitterUrl,
+//         location: req.body.location,
+//      photoUrl: req.body.photoUrl
+//   })
+//   newUser.save()
+//   .then(()=>{
+//       console.log("Saved new user to the database")
+//       res.redirect('/users')
+//   })
+//   .catch((err)=>{
+//       console.log(err)
+//   })
+// })
+
+// router.get('/:userId', (req, res) => {
+//   const userId = req.params.userId
+  
+//   User.findById(userId)
+//     .then((user) => {
+//       res.render('users/show', {
+//           user
+//       })
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+// })
+
+// router.get('/:userId/edit', (req,res)=>{
+//     const userId = req.params.userId 
+//     console.log(userId)
+//     User.findById(userId)
+//     .then((user)=>{
+//         res.render('users/edit', {
+//             user
+//         })
+//     })
+// })
+
+
+// router.put('/:userId', (req, res)=>{
+//   const userId = req.params.userId
+//   const updatedUserInfo = req.body
+
+//     User.findByIdAndUpdate(userId, updatedUserInfo)
+//     .then(()=>{
+//         res.redirect(`/users/${userId}`)
+//     })
+//     .catch((err)=>{
+//         console.log(err)
+//     })
+// })
+
+// router.get('/:userId/delete', (req, res) => {
+//   const userId = req.params.userId
+
+//   User.findByIdAndRemove(userId)
+//   .then(()=>{
+//       res.redirect('/users')
+//   })
+//   .catch((err)=>{
+//       console.log(err)
+//   })
+// })
 
 
 
 
 
 
-
-
-
-
-module.exports = router
+  module.exports = router
