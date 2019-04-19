ab58c536ef1d2d8ce45bf3f3e5ccb650d61fd472
jxa8hst <jean_altidor@homdepot.com>
2019-04-18 21:09:37 -0400

    removed unsued LOC, using es6

--- server.js
+++ server.js
@@ -1,33 +1,23 @@
-require('dotenv').config();
-import { config } from 'dotenv';
-import cookieParser from 'cookie-parser'
-import bodyParser from 'body-parser'
-import express from 'express'
-import { MongoClient} from 'mongodb';
-import mongoose from 'mongoose'
-import path from 'path'
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
 
-import logger from './util/logger'
-import indexController from './routes/indexController'
-import styleController from './routes/styleController'
-import userController from './routes/userController'
-import influencerController from './routes/influencerController.js'
+import logger from './util/logger';
+import styleController from './routes/styleController';
+import userController from './routes/userController';
+import influencerController from './routes/influencerController';
+
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 1748;
 
-const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-    const database = client.db("sample_airbnb")
-    const collection = database.collection("listingsAndReviews");
-    console.log(collection)
-  // perform actions on the collection object
-//   console.log(collection)
-  client.close();
-});
-
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Methods', 'OPTIONS');
@@ -35,84 +25,28 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.set('view engine', 'hbs');
-// app.set('views', path.join(__dirname, 'views'));
 
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
-
-
-// app.use(methodOverride('_method'));
-
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI);
-
-
-// mongoose.connection.once('open', () => {
-//     console.log('Mongoose has connected to MongoDB!');
-// });
-
-// mongoose.connection.on('error', (error) => {
-//     console.error(`
-//     MongoDB connection error!!! 
-//     ${ error }
-//   `);
-//     process.exit(-1);
-// });
-// app.get('/', (request, response) => {
-//     response.redirect('/users');
-// });
-
-
-
-
-app.use('/', async () => {
-    await db.collection('inventory').insertMany([
-        {
-          item: 'journal',
-          qty: 25,
-          tags: ['blank', 'red'],
-          size: { h: 14, w: 21, uom: 'cm' }
-        },
-        {
-          item: 'mat',
-          qty: 85,
-          tags: ['gray'],
-          size: { h: 27.9, w: 35.5, uom: 'cm' }
-        },
-        {
-          item: 'mousepad',
-          qty: 25,
-          tags: ['gel', 'blue'],
-          size: { h: 19, w: 22.85, uom: 'cm' }
-        }
-      ]);
+mongoose.connection.once('open', () => {
+    logger.info('Mongoose has connected to MongoDB!');
 });
+
+mongoose.connection.on('error', (error) => {
+    logger.error(`
+    MongoDB connection error!!! 
+    ${ error }
+  `);
+    process.exit(-1);
+});
+
+
 app.use('/users', userController);
 app.use('/users/:userId/influencer', influencerController);
 app.use('/users/:userId/influencer/:influencerId/style', styleController);
 
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
-
-// app.use((req, res, next) => {
-//     const err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
-
-// app.use((err, req, res) => {
-//     res.locals.message = err.message;
-//     res.locals.error = req.app.get('env') === 'development' ? err : {};
-//     res.status(err.status || 500);
-//     res.render('error');
-// });
-
-// module.exports = app;
-const PORT = process.env.PORT || 1748;
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${ PORT}`)
-})
+    logger.info(`Server running on port: ${ PORT }`);
+});
