1985bd47924a4121ed71d212af51b153e2184d9f
jxa8hst <jean_altidor@homdepot.com>
2019-04-18 21:10:38 -0400

    eslint fix

--- db/schema.js
+++ db/schema.js
@@ -1,75 +1,76 @@
-import mongoose from 'mongoose'
-const Schema = mongoose.Schema
+import mongoose from 'mongoose';
 
-//promises "then" and "catch" variables
+const Schema = mongoose.Schema;
+
+// promises "then" and "catch" variables
 mongoose.Promise = global.Promise;
 const StyleSchema = new Schema({
-  img1: {
-    type: String
-  },
-  img2: {
-    type: String
-  }
+    img1: {
+        type: String
+    },
+    img2: {
+        type: String
+    }
 }, {
-  timestamps: {},
-  usePushEach: true
-})
+    timestamps: {},
+    usePushEach: true
+});
 const InfluencerSchema = new Schema({
-  name: {
-    type: String
-  },
-  instagram: {
-    type: String
-  },
-  location: {
-    type: String
-  },
-  photoUrl: {
-    type: String
-  },
-  style: [StyleSchema]
+    name: {
+        type: String
+    },
+    instagram: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    photoUrl: {
+        type: String
+    },
+    style: [StyleSchema]
 }, {
-  timestamps: {},
-  usePushEach: true
-})
+    timestamps: {},
+    usePushEach: true
+});
 const UserSchema = new Schema({
-  username: {
-    type: String
-  },
-  firstName: {
-    type: String
-  },
-  lastName: {
-    type: String
-  },
-  Userinstagram: {
-    type: String
-  },
-  twitter: {
-    type: String
-  },
-  location: {
-    type: String
-  },
-  photoUrl: {
-    type: String,
-    default: 'http://www.clipartlord.com/wp-content/uploads/2014/04/egg4.png'
-  },
-  description: {
-    type: String
-  },
-  motto: {
-    type: String
-  },
+    username: {
+        type: String
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    instagram: {
+        type: String
+    },
+    twitter: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    photoUrl: {
+        type: String,
+        default: 'http://www.clipartlord.com/wp-content/uploads/2014/04/egg4.png'
+    },
+    description: {
+        type: String
+    },
+    motto: {
+        type: String
+    },
 
-  influencers: [InfluencerSchema]
+    influencers: [InfluencerSchema]
 }, {
-  timestamps: {},
-  usePushEach: true
-})
+    timestamps: {},
+    usePushEach: true
+});
 
 export {
-  UserSchema,
-  StyleSchema,
-  InfluencerSchema
-}
+    UserSchema,
+    StyleSchema,
+    InfluencerSchema
+};
