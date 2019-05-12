b21c7b3232cf4cb2a51497c959c7a17e6e10e6c7
jxa8hst <jean_altidor@homdepot.com>
2019-05-12 15:05:25 -0400

    client side routes

---
+++ client/src/components/Routes/index.js
@@ -0,0 +1,7 @@
+export const LANDING = '/'
+export const USERS = '/users'
+export const USER_BY_ID = '/users:ID'
+export const USER_BY_ID_STYLES = '/users:ID/styles'
+
+export const INFLUENCERS = '/influencers'
+export const INFLUENCERSBYID = '/influencers:ID'
